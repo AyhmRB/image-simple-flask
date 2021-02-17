d29e550a1fa4aae0639f5fdd7ad78852f5bd3ce1
Ayhm <44723034+AyhmRB@users.noreply.github.com>
2021-02-17 14:33:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-#Additional Tests
+
+#------------------------------------------Additional Tests
+#Additional Tests------------------------------------------
 @app.route("/callthis")
 def test():
     return "<h1> hello there </h1>"
+
 
 @app.route("/testjson")
 def testjson(): 
@@ -21,25 +24,57 @@ def testjson():
         "test2":"valuessdsaf"
         }
     return jsonify(x)
-#Additional Tests
 
 @app.route("/ai", methods = ["POST"])
 def predict():
     
     lr = request.args.get('data')
-    lr = lr.split(',')
-    lr = np.array(lr).astype('float64')
-    
-    model = joblib.load('ai_test.joblib')
-    
-    y = model.predict(np.array([lr]).reshape(-1,1))
-    
-    
-    y = str(y)
-    
-    return y
+    try:
+        lr = lr.split(',')
+        lr = np.array(lr).astype('float64')
+        
+        model = joblib.load('ai_test.joblib')
+        
+        y = model.predict(np.array([lr]).reshape(-1,1))
+        
+        
+        y = str(y)
+        
+        return y
+    except:
+        return '<h1> Error in Handling Data </h1>'
+#------------------------------------------Additional Tests
+#Additional Tests------------------------------------------
 
+@app.route("/")
+def index():
+    return render_template("upload.html")
 
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+    if not os.path.isdir(target):
+            os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("Accept incoming file:", filename)
+        print ("Save it to:", destination)
+        upload.save(destination)
+
+    return render_template("complete.html", image_name=filename)
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
 
 if __name__ == "__main__":
     app.run()
+    
+
